c613ae9b48e60b4eee360512db671d2ec281d222
radiopunch <radiopanch1221@gmail.com>
2022-03-13 21:34:32 +0900

    最低限の実装

---
+++ script.js
@@ -0,0 +1,30 @@
+let winstreak = 0;
+
+function update(){
+  if(winstreak<0){
+    winstreak = 0;
+  }
+  document.getElementById("winstreak").innerText = winstreak.toString()+"連勝";
+  lap = Math.floor(winstreak/7)+1
+  num = winstreak%7+1
+  document.getElementById("detail").innerText = lap.toString()+"週目"+num.toString()+"戦目";
+}
+
+function increase() {
+  winstreak = winstreak+1;
+  update();
+}
+
+function decrease() {
+  winstreak = winstreak-1;
+  update();
+}
+
+function reset() {
+  winstreak = 0;
+  update();
+}
+
+document.getElementById("increase").addEventListener("click", increase);
+document.getElementById("decrease").addEventListener("click", decrease);
+document.getElementById("reset").addEventListener("click", reset);
